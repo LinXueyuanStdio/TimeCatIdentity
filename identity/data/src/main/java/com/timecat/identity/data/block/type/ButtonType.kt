61355d1f740ef561a554599438d88daaed7515ad
linxueyuanstdio <761516186@qq.com>
2021-08-28 13:03:10 +0800

    Update ButtonType.kt

--- identity/data/src/main/java/com/timecat/identity/data/block/type/ButtonType.kt
+++ identity/data/src/main/java/com/timecat/identity/data/block/type/ButtonType.kt
@@ -10,10 +10,21 @@ import androidx.annotation.IntDef
  * @usage null
  */
 @IntDef(
+    BUTTON_Stop,
+    BUTTON_Notifier,
+    BUTTON_Timer,
     BUTTON_Duplicate,
+    BUTTON_Loop,
+    BUTTON_Runner,
+    BUTTON_AbsoluteRunner,
 )
 @Retention(AnnotationRetention.SOURCE)
 annotation class ButtonType
 
-const val BUTTON_Duplicate: Int = 0 //复读机
-
+const val BUTTON_Stop = 0 //直接终止，啥也不干
+const val BUTTON_Notifier = 1 //通知器，多种通知手段
+const val BUTTON_Timer = 2 //计时器
+const val BUTTON_Duplicate = 3 //复读机
+const val BUTTON_Loop = 4 //循环机
+const val BUTTON_Runner = 5 //执行者，可自动组合执行其他机械符文
+const val BUTTON_AbsoluteRunner = 6 //绝对执行者，在绝对时间点自动执行指定的机械符文
