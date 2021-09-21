1508fc9ea609e3819ab1b8d6266f70c8e6b2ce64
linxueyuanstdio <761516186@qq.com>
2021-09-22 00:22:15 +0800

    s

--- identity/service/src/main/java/com/timecat/identity/service/BubbleService.java
+++ identity/service/src/main/java/com/timecat/identity/service/BubbleService.java
@@ -2,6 +2,9 @@ package com.timecat.identity.service;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+
+import androidx.annotation.Nullable;
 
 /**
  * @author 林学渊
@@ -11,7 +14,57 @@ import android.graphics.Bitmap;
  * @usage 单击触发
  */
 public interface BubbleService {
-    void show(Bitmap drawable, String title, String content, Listener listener);
+    /**
+     * 显示气泡
+     * 完整内容为 "【title】content"
+     * @param bitmap 头像
+     * @param title 标题 
+     * @param content 内容
+     * @param listener 点击气泡后触发
+     */
+    void show(@Nullable Bitmap bitmap, String title, String content, Listener listener);
+
+    /**
+     * 显示气泡
+     * @param bitmap 头像
+     * @param content 内容
+     * @param listener 点击气泡后触发
+     */
+    void show(@Nullable Bitmap bitmap, String content, Listener listener);
+
+    /**
+     * 显示气泡
+     * 完整内容为 "【title】content"
+     * @param drawable 头像
+     * @param title 标题 
+     * @param content 内容
+     * @param listener 点击气泡后触发
+     */
+    void show(@Nullable Drawable drawable, String title, String content, DanmuService.Listener listener);
+
+    /**
+     * 显示气泡
+     * @param drawable 头像
+     * @param content 内容
+     * @param listener 点击气泡后触发
+     */
+    void show(@Nullable Drawable drawable, String content, DanmuService.Listener listener);
+
+    /**
+     * 显示气泡，默认头像为应用图标
+     * 完整内容为 "【title】content"
+     * @param title 标题 
+     * @param content 内容
+     * @param listener 点击气泡后触发
+     */
+    void show(String title, String content, Listener listener);
+
+    /**
+     * 显示气泡
+     * @param content 内容
+     * @param listener 点击气泡后触发
+     */
+    void show(String content, Listener listener);
 
     interface Listener {
         void OnClick(Context context);
--- identity/service/src/main/java/com/timecat/identity/service/DanmuService.java
+++ identity/service/src/main/java/com/timecat/identity/service/DanmuService.java
@@ -10,63 +10,63 @@ import androidx.annotation.Nullable;
  * @author 林学渊
  * @email linxy59@mail2.sysu.edu.cn
  * @date 2020/5/31
- * @description 磁贴通知服务。磁贴通知必须手动取消才会消失
- * @usage 单击展开收起，长按触发
+ * @description 弹幕通知服务。弹幕通知 3 秒后自动消失
+ * @usage 单击触发
  */
-public interface TileService {
+public interface DanmuService {
     /**
-     * 显示磁贴
+     * 显示弹幕
      * 完整内容为 "【title】content"
      * @param bitmap 头像
      * @param title 标题 
      * @param content 内容
-     * @param listener 点击磁贴后触发
+     * @param listener 点击弹幕后触发
      */
     void show(@Nullable Bitmap bitmap, String title, String content, Listener listener);
 
     /**
-     * 显示磁贴
+     * 显示弹幕
      * @param bitmap 头像
      * @param content 内容
-     * @param listener 点击磁贴后触发
+     * @param listener 点击弹幕后触发
      */
     void show(@Nullable Bitmap bitmap, String content, Listener listener);
 
     /**
-     * 显示磁贴
+     * 显示弹幕
      * 完整内容为 "【title】content"
      * @param drawable 头像
-     * @param title 标题 
+     * @param title 标题
      * @param content 内容
-     * @param listener 点击磁贴后触发
+     * @param listener 点击弹幕后触发
      */
-    void show(@Nullable Drawable drawable, String title, String content, DanmuService.Listener listener);
+    void show(@Nullable Drawable drawable, String title, String content, Listener listener);
 
     /**
-     * 显示磁贴
+     * 显示弹幕
      * @param drawable 头像
      * @param content 内容
-     * @param listener 点击磁贴后触发
+     * @param listener 点击弹幕后触发
      */
-    void show(@Nullable Drawable drawable, String content, DanmuService.Listener listener);
+    void show(@Nullable Drawable drawable, String content, Listener listener);
 
     /**
-     * 显示磁贴，默认头像为应用图标
+     * 显示弹幕，默认头像为应用图标
      * 完整内容为 "【title】content"
      * @param title 标题 
      * @param content 内容
-     * @param listener 点击磁贴后触发
+     * @param listener 点击弹幕后触发
      */
     void show(String title, String content, Listener listener);
 
     /**
-     * 显示磁贴
+     * 显示弹幕
      * @param content 内容
-     * @param listener 点击磁贴后触发
+     * @param listener 点击弹幕后触发
      */
     void show(String content, Listener listener);
 
     interface Listener {
-        void OnLongClick(Context context);
+        void OnClick(Context context);
     }
 }
